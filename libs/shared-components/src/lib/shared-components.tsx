8e7c636705f638199875e39890743741dbbe9091
ahmedumerr <ahmed.umer@arbisoft.com>
2025-11-26 04:04:23 +0500

    add apps and libraries through nx generator

---
+++ libs/shared-components/src/lib/shared-components.tsx
@@ -0,0 +1,11 @@
+import styles from './shared-components.module.scss';
+
+export function SharedComponents() {
+  return (
+    <div className={styles['container']}>
+      <h1>Welcome to SharedComponents!</h1>
+    </div>
+  );
+}
+
+export default SharedComponents;
